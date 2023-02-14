940e37e7a48d8f822d7f7ece271ea90ddeb2b20d
Firdaus Fitri <dausfitri@DausFitri-MBP.local>
2023-02-14 21:06:50 +0800

    add files

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+Assignment
+1. input kredit awal, kredit yg ingin ditambah dan hutang yg ada, dan outputkan kredit akhir
+2. outputkan hari program ni dibuka mengikut hari di pc user tersebut "today is "
+
+*/
+
+
+prompt("")
