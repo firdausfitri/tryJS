19300ba5faabdf4cef6bfdaa4f5a2987d56eeaf2
Firdaus Fitri <dausfitri@DausFitri-MBP.local>
2023-02-14 21:41:28 +0800

    updated v

--- main.js
+++ main.js
@@ -6,4 +6,47 @@ Assignment
 */
 
 
-var initCredit = prompt("What is your initial credit?")
+var num1 = prompt("What is your initial credit?")
+var num2 = prompt("How much credit you want to add?")
+
+var initCredit = parseInt(num1)
+var addCredit = parseInt(num2)
+const totalCredit = initCredit+addCredit
+
+alert(`Your credit is ${totalCredit}`)
+
+
+let d = new Date()
+let day = d.getDay()
+
+switch (day) {
+    case 1:
+        console.log("Today is Monday")
+        break
+    case 2:
+        console.log("Today is Tuesday")
+        break
+
+    case 3:
+        console.log("Today is Wednesday")
+        break
+
+    case 4:
+        console.log("Today is Thursday")
+        break
+
+    case 5:
+        console.log("Today is Friday")
+        break
+
+    case 6:
+        console.log("Today is Saturday")
+        break
+
+    case 7:
+        console.log("Today is Sunday")
+        break
+    default:
+        console.log("Day invalid")
+
+}
